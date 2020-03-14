ec67e0657c677d02fafec3e201c0cdd13216f8f4
Kevin Pacheco <k.pacheco@me.com>
2020-03-14 11:04:50 -0700

    Porting Snip to OSX

---
+++ Snip.OSX/AppDelegate.cs
@@ -0,0 +1,43 @@
+﻿using AppKit;
+using Foundation;
+
+namespace Snip.OSX {
+	using System.Runtime.CompilerServices;
+	[Register("AppDelegate")]
+	public class AppDelegate : NSApplicationDelegate {
+		NSStatusItem item;
+		bool iTunes;
+
+		public AppDelegate() {
+
+		}
+
+		public override void DidFinishLaunching(NSNotification notification) {
+			// Insert code here to initialize your application
+			var statusBar = NSStatusBar.SystemStatusBar;
+
+			this.item = statusBar.CreateStatusItem(NSStatusItemLength.Variable);
+
+			this.item.Button.Image = new NSImage("Snip16.png");
+			this.item.ToolTip = "CMD + Click to quit";
+			item.Action = new ObjCRuntime.Selector("itemClicked");
+			item.Target = this;
+
+			NSMenu menu = new NSMenu("Title");
+
+			menu.AddItem(new NSMenuItem("Item 1", new System.EventHandler((o, e) => { this.itemClicked(); })));
+
+			this.item.Menu = menu;
+		}
+
+		public override void WillTerminate(NSNotification notification) {
+			// Insert code here to tear down your application
+		}
+
+		[Export("itemClicked")]
+		private void itemClicked() {
+			NSAlert.WithMessage("Hello, World!", "OK", "", "", "").RunModal();
+			System.Diagnostics.Debug.WriteLine("Action!");
+		}
+	}
+}
---
+++ Snip.OSX/Main.cs
@@ -0,0 +1,10 @@
+﻿using AppKit;
+
+namespace Snip.OSX {
+	static class MainClass {
+		static void Main(string[] args) {
+			NSApplication.Init();
+			NSApplication.Main(args);
+		}
+	}
+}
